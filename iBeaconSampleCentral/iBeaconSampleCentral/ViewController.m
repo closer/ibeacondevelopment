76e0e19e6a511cfdfc04ebb0ada69f9500a4eac1
sibatomo <tomoki@exfrontier.com>
2014-06-07 15:56:41 +0900

    add to Send HTTP Request explode Unknown state from notification

--- iBeaconSampleCentral/iBeaconSampleCentral/ViewController.m
+++ iBeaconSampleCentral/iBeaconSampleCentral/ViewController.m
@@ -14,7 +14,7 @@
 @property (nonatomic) CLLocationManager *locationManager;
 @property (nonatomic) NSUUID *proximityUUID;
 @property (nonatomic) CLBeaconRegion *beaconRegion;
-
+@property (nonatomic) NSUUID *venderUUID;
 @end
 
 @implementation ViewController
@@ -34,6 +34,8 @@
                                                                identifier:@"com.otoshimono.testregion"];
         [self.locationManager startMonitoringForRegion:self.beaconRegion];
     }
+    _venderUUID = [UIDevice currentDevice].identifierForVendor;
+
 }
 
 - (void)didReceiveMemoryWarning
@@ -47,8 +49,8 @@
 - (void)locationManager:(CLLocationManager *)manager didStartMonitoringForRegion:(CLRegion *)region
 {
     [self sendLocalNotificationForMessage:@"Start Monitoring Region"];
-    
     NSLog(@"Start Monitoring Region");
+    [self sendNSURLRequest:[NSString stringWithFormat:@"http://lab.exer.jp/tag/regist/userid/%@/state/startMonitroing/",_venderUUID.UUIDString]];
     [self.locationManager requestStateForRegion:self.beaconRegion];
 }
 
@@ -58,12 +60,15 @@
         case CLRegionStateInside: // リージョン内にいる
             [self sendLocalNotificationForMessage:@"I'm standing by you!!"];
             NSLog(@"I'm standing by you!!");
+            [self sendNSURLRequest:[NSString stringWithFormat:@"http://lab.exer.jp/tag/regist/userid/%@/state/inside/",_venderUUID.UUIDString]];
             break;
         case CLRegionStateOutside:
-        case CLRegionStateUnknown:
             [self sendLocalNotificationForMessage:@"Where you are??"];
             NSLog(@"Where you are??");
+            [self sendNSURLRequest:[NSString stringWithFormat:@"http://lab.exer.jp/tag/regist/userid/%@/state/ousside/",_venderUUID.UUIDString]];
+
             break;
+        case CLRegionStateUnknown:
         default:
             break;
     }
@@ -72,23 +77,58 @@
 - (void)locationManager:(CLLocationManager *)manager didEnterRegion:(CLRegion *)region
 {
     [self sendLocalNotificationForMessage:@"Enter Region"];
-    
     NSLog(@"Enter Region");
+    [self sendNSURLRequest:[NSString stringWithFormat:@"http://lab.exer.jp/tag/regist/userid/%@/state/enter/",_venderUUID.UUIDString]];
 }
 
 - (void)locationManager:(CLLocationManager *)manager didExitRegion:(CLRegion *)region
 {
     [self sendLocalNotificationForMessage:@"Exit Region"];
     NSLog(@"Exit Region");
+    [self sendNSURLRequest:[NSString stringWithFormat:@"http://lab.exer.jp/tag/regist/userid/%@/state/exit/",_venderUUID.UUIDString]];
 }
 
 - (void)locationManager:(CLLocationManager *)manager monitoringDidFailForRegion:(CLRegion *)region withError:(NSError *)error
 {
     [self sendLocalNotificationForMessage:@"RegioningFailed"];
     NSLog(@"RegionFailed");
+    [self sendNSURLRequest:[NSString stringWithFormat:@"http://lab.exer.jp/tag/regist/userid/%@/state/regionFailed/",_venderUUID.UUIDString]];
 }
 
 #pragma mark - Private methods
+
+- (void)sendNSURLRequest:(NSString *)url
+{
+    NSURLRequest *request = [[NSURLRequest alloc] initWithURL:[NSURL URLWithString:url]];
+    [NSURLConnection sendAsynchronousRequest:request queue:[[NSOperationQueue alloc] init] completionHandler:^(NSURLResponse *response, NSData *data, NSError *error) {
+
+        if (error) {
+            if (error.code == -1003) {
+                NSLog(@"not found hostname. targetURL=%@", url);
+            } else if (-1019) {
+                NSLog(@"auth error. reason=%@", error);
+            } else {
+                NSLog(@"unknown error occurred. reason = %@", error);
+            }
+            
+        } else {
+            long httpStatusCode = ((NSHTTPURLResponse *)response).statusCode;
+            if (httpStatusCode == 404) {
+                NSLog(@"404 NOT FOUND ERROR. targetURL=%@", url);
+                // } else if (・・・) {
+                // 他にも処理したいHTTPステータスがあれば書く。
+            } else {
+                NSLog(@"success request!!");
+                NSLog(@"statusCode = %ld", (long)((NSHTTPURLResponse *)response).statusCode);
+                NSLog(@"responseText = %@", [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding]);
+                // ここはサブスレッドなので、メインスレッドで何かしたい場合には
+                dispatch_async(dispatch_get_main_queue(), ^{
+                    // ここに何か処理を書く。
+                });
+            }
+        }
+    }];
+}
 
 
 - (void)sendLocalNotificationForMessage:(NSString *)message
